447ae22370a7bae46afee6a0440c1b6f8d61f4b9
Albina Khanbekova <albina.khanbekova@akvelon.com>
2018-07-27 18:55:45 +0300

    add exchange rates

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ApolloProvider } from "react-apollo";
 import client from './apollo-client';
 import logo from './logo.svg';
 import './App.css';
+import ExchangeRates from './ExchangeRates/ExchangeRates';
 
 
 class App extends Component {
@@ -16,7 +17,8 @@ class App extends Component {
           </header>
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          </p>
+          <ExchangeRates />
         </div>
       </ApolloProvider>
     );
